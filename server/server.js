e365982acd4d4d74a0c41c26100b31624de9fda1
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-04-07 16:27:05 -0400

    PORT

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, './../')));
 
@@ -8,6 +9,6 @@ app.get('/', function(req,res) {
   res.sendFile('/index.html');
 });
 
-app.listen(3000, function() {
-  console.log('Server is listening on port 3000');
+app.listen(PORT, function() {
+  console.log('Server is listening on port '+PORT);
 });
