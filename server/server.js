c363cf51156e871fc4fd7ce55770f1a9b7cc24bb
Kaley <kaleysullivan@KALEYs-MacBook-Pro.local>
2016-04-08 11:31:49 -0400

    the Ss have it... https makes everything better

--- server/server.js
+++ server/server.js
@@ -3,16 +3,29 @@ var path = require('path');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, './../')));
+// app.use(express.static(path.join(__dirname, './../')));
 var PORT = process.env.PORT || 3000;
 
-//OAUTH STUFF
+
+//HEROKU
+// var credentials = {
+//  clientID: "34470edff3fc9aac0c6895b6c0fab87e27f026c33bf8a2015b2dc51905032012",
+//  clientSecret: "8fe080b72de0b753613d3c6397aadce961ca8f524068480eec8dbf42f607b1e9",
+//  site: "http://recurse.com",
+//  redirect_uri: 'https://rccheckins.herokuapp.com/callback'
+// }
+
+//TESTING
 var credentials = {
- clientID: "34470edff3fc9aac0c6895b6c0fab87e27f026c33bf8a2015b2dc51905032012",
- clientSecret: "8fe080b72de0b753613d3c6397aadce961ca8f524068480eec8dbf42f607b1e9",
- site: "http://recurse.com",
- redirect_uri: 'https://rccheckins.herokuapp.com/callback'
+ clientID: "207e556737f394a07703264dc1193bb711538ce24da5ef43b52a322f76e8d47d",
+ clientSecret: "398626c53eb32d662c9aa66ad8051b5cef148d155f69be9cb3e3d47fc0eb1004",
+ site: "https://recurse.com",
+ redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
+ tokenPath: "/oauth/token",
+ authorizationPath: "/oauth/authorize"
 }
+
+
 
 var oauth = require('simple-oauth2')(credentials)
 
@@ -32,20 +45,26 @@ app.get('/auth', function (req, res) {
 
 var token; 
 
+
+
+
 // Callback service parsing the authorization token and asking for the access token
 app.get('/callback', function (req, res) {
 	console.log("/callback")
   var code = req.query.code;
+  console.log("Code ", code)
 
   oauth.authCode.getToken({
     code: code,
     redirect_uri: credentials.redirect_uri
   }, saveToken);
 
+
+   
+
   function saveToken(error, result) {
     if (error) { console.log('Access Token Error', error.message); }
     token = oauth.accessToken.create(result);
-    console.log("TOKEN IS ",oauth.accessToken)
   }
 
   res.send("TEST")
